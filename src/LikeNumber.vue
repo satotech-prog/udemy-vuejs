30e419bd930c85cab06e7a106bcb3f0e1ff74d6c
satotech-prog <72504810+satotech-prog@users.noreply.github.com>
2021-01-04 20:45:50 +0900

    firstcommit

---
+++ src/LikeNumber.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+  <p>いいね({{number}})</p>
+<button @click="include">+1</button>
+</div>
+
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      number: 5
+    };
+  },
+methods: {
+  include () {
+    this.number += 1
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
+
