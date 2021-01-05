d3771cdd265cee5f0df2692a37c57222e0c398ee
satotech-prog <72504810+satotech-prog@users.noreply.github.com>
2021-01-05 21:57:02 +0900

    ローカル登録でheader作成

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,21 @@
 <template>
-<LikeNumber></LikeNumber>
+  <div>
+    <LikeHeader></LikeHeader>
+    <LikeNumber></LikeNumber>
+  </div>
 </template>
+
+<script>
+import LikeHeader from "./LikeHeader.vue";
+
+
+export default {
+  components: {
+    LikeHeader: LikeHeader
+  }
+}
+
+
+</script>
+
 
